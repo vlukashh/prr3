4d9eaba15f672508ece70dace4513d2745889538
vlukashh <ankalukashenko@gmail.ru>
2023-11-20 21:09:06 +0700

    Добавление возможности аутентификации

--- tutorial/snippets/urls.py
+++ tutorial/snippets/urls.py
@@ -1,4 +1,4 @@
-from django.urls import path
+from django.urls import path, include
 from rest_framework.urlpatterns import format_suffix_patterns
 
 from . import views
@@ -6,8 +6,11 @@ from . import views
 urlpatterns = [
    path('snippets/', views.SnippetList.as_view()),
    path('snippets/<int:pk>/', views.SnippetDetail.as_view()),
+   path('users/', views.UserList.as_view()),
+   path('users/<int:pk>/', views.UserDetail.as_view()),
+   path('auth/', include('rest_framework.urls')),
 ]
 
-
 urlpatterns = format_suffix_patterns(urlpatterns)
+
 
